fce7ebef3d629d6e1cfe2c68971c60a78e9af31a
Amorem <dev@amorem.com>
2020-11-24 19:02:01 +0100

    use chalk package

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 // chmod +x index.js
 
+const chalk = require("chalk");
 const welcome = require("cli-welcome");
+
 const pkgJSON = require("./package.json");
 
+// ShortHand
+const log = console.log;
+const twitterStyle = chalk.hex("#1da1f2").bold;
+const githubStyle = chalk.hex("#6cc644").bold;
+const purpleStyle = chalk.hex("#6937FF").bold;
+
 welcome({
-  title: pkgJSON.name,
+  title: "Amorem",
   tagLine: "Howdy, nice to meet you !",
   description: pkgJSON.description,
   version: pkgJSON.version,
@@ -15,4 +23,10 @@ welcome({
   clear: true,
 });
 
-console.log("Hello Cedric");
+log(`${chalk.bgBlue(` Amorem `)}`);
+
+log(`${chalk.dim.italic("New string")}`);
+
+log(`${twitterStyle("Twitter")}`);
+log(`${githubStyle("Github")}`);
+log(`${blogStyle("Blog")}`);
