66856b193d57b0ff3d7e1d1fcda3bb4e2362a59a
Amorem <dev@amorem.com>
2020-11-24 15:42:40 +0100

    Add cli-welcome package

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 // chmod +x index.js
 
-const clearConsole = require("clear-any-console");
+const welcome = require("cli-welcome");
+const pkgJSON = require("./package.json");
 
-clearConsole();
+welcome({
+  title: pkgJSON.name,
+  tagLine: "Howdy, nice to meet you !",
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#FADC00`,
+  color: "#000000",
+  bold: true,
+  clear: true,
+});
+
 console.log("Hello Cedric");
